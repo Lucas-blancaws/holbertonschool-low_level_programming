1954e4996c95ad40883796a3e627e338558734fd
Lucas-blancaws <lblancportier@gmail.com>
2025-02-17 10:27:06 +0000

    4. Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 /**
- * function_name - Short description, single line
- * @parameterx: Description of parameter x
-(* a blank line
- * Description: Longer description of the function)?
-(* section header: Section description)*
- * Return: Description of the returned value
+ * main - Short description, single line
+ * Description: Flemme
+ * Return: valeur O
  */
 int main(void)
 {
