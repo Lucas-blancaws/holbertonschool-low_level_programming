e43373d05850e1bd59faf41bab2c7c871a97c5fa
Lucas-blancaws <lblancportier@gmail.com>
2025-02-17 10:37:18 +0000

    5. Hello, printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - Point d'entree
- * Descriptione: Flemme 
- * Return:valeur 0
- */
+* main - Point d'entree
+* Descriptione: Flemme
+* Return:valeur 0
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
