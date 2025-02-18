2ac33469a52aab99bc3f1b575ca8d1f86621f164
Lucas-blancaws <lblancportier@gmail.com>
2025-02-18 14:04:23 +0000

    7. Smile in the mirror

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Alphabet a l'envers
+* Description : lire au-dessus
+* Return: Always (Success)
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c <= 'a'; c--)
+	{
+		putchar (c);
+	}
+	putchar ('\n');
+	return (0);
+}
