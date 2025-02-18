37e1fe18d5e7379c31cd0ab10e7bcad8cb95f9dd
Lucas-blancaws <lblancportier@gmail.com>
2025-02-18 13:47:12 +0000

    5. Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - lire description
+* Description : affiche les nombres jusqu'a dix
+* Return: Always (Success)
+*/
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+
+	{
+		putchar (n);
+	}
+	putchar('\n');
+		return (0);
+}
