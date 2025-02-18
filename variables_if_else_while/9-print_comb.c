356db3f71258d776bcf58b5d3a5f1a34ca1fbfb1
Lucas-blancaws <lblancportier@gmail.com>
2025-02-18 14:56:28 +0000

    9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main- affiche toutes les combinaiaisons de de nombre a un chiffre
+* Description : juste au dessus
+* Return: Always (Success)
+*/
+
+int main(void)
+{
+int num;
+
+for (num = '0'; num <= '9'; num++)
+{
+	putchar (num);
+	putchar (',');
+	putchar (' ');
+}
+putchar ('\n');
+return (0);
+}
