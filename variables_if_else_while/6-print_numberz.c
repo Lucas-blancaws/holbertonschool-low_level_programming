892731c2f258f2feecdae1aba9c70c97229b8e67
Lucas-blancaws <lblancportier@gmail.com>
2025-02-18 13:53:53 +0000

    6. Numberz

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -6,13 +6,13 @@
 */
 int main(void)
 {
-        int n;
+int n;
 
-        for (n = '0'; n <= '9'; n++)
+for (n = '0'; n <= '9'; n++)
 
-        {
-                putchar (n);
-        }
-        putchar('\n');
-                return (0);
+{
+putchar (n);
+}
+putchar('\n');
+return (0);
 }
