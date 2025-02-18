5a94910160fcdfae6c2a1fb834847a34cc364061
Lucas-blancaws <lblancportier@gmail.com>
2025-02-18 10:40:47 +0000

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
-
+/**
+* main - Point d'entree
+* Description: ALphabet de a-z avec 2 putchar
+* Return: valeur O
+*/
 int main(void)
 {
-putchar('a');
-putchar('z');
+int a = 97;
+while (a < 123)
+{
+putchar(a);
+a++;
+}
+putchar('\n');
 return (0);
 }
