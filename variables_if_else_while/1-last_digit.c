4216e2e716ed7916630782ec9a9ce83413f2d297
Lucas-blancaws <lblancportier@gmail.com>
2025-02-18 09:49:25 +0000

    1. The last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,22 +1,23 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /**
 * main - Point d'entree
-* Description: Flemme
+* Description: Affiche le dernier chiffre d'un nombre aletoire
 * Return: valeur O
 */
 
 int main(void)
 {
-	int n;
-	
+  int n, lastd;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 5)
-	printf ("Last digit of %d is %d and is greater than 5\n", n);
-	else if
-	printf ("Last digit of %d is %d and is 0\n", n);
-	else if
-	printf ("Last digit of %d is %d and is less than 6 and not 0\n", n);
+	lastd = n % 10;
+	if (lastd > 5)
+	printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
+	else if (lastd == 0)
+	printf("Last digit of %d is %d and is 0\n", n, lastd);
+	else (n < 6)
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
 	return (0);
 }
