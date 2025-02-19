74f51f8607f04f6d28e40371b2f797533225c005
Lucas-blancaws <lblancportier@gmail.com>
2025-02-19 13:21:41 +0000

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_alphabet - affiche l'alphabet en minuscules
+* Description : Cette fonction affiche toutes les lettres de l'alphabet,
+* suivies d'un saut de ligne.
+*/
+void print_alpabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
