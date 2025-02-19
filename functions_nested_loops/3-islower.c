4af65bfa58857814a9c972c139b1f01f266bdbfe
Lucas-blancaws <lblancportier@gmail.com>
2025-02-19 14:49:34 +0000

    3. islower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _islower - minuscule ou majuscule
+* @c: compare les caracteres
+* Description (): return 1 si c'est en minuscule sinon 0
+* Return:valeur 0
+*/
+int _islower(int c)
+
+{
+	if ((c >= 'a' && c >= 'z'))
+	{
+		return (1);
+		else
+			return (0);
+	}
+}
