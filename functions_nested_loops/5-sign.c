fa644b2cf29a870f9ee4fe95889d625f60b15da1
Lucas-blancaws <lblancportier@gmail.com>
2025-02-20 13:35:50 +0000

    5. Sign

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* print_sign - +, - ou 0
+* c: caract
+* Description ():return 1 pour les num +, 0 pour 0 et -1 pour les num -
+* Return:valeur 0
+*/
+
+int print_sign(int n)
+
+{
+	if (n > '0')
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar ('0');
+		return (0);
+	}
+	else
+	{
+		_putchar ('-');
+		return (-1);
+	}
+}
