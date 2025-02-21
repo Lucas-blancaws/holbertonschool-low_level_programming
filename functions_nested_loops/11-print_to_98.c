d225819b512a3eaf7a0b4d43375bd7aeed5ec6af
Lucas-blancaws <lblancportier@gmail.com>
2025-02-21 13:33:33 +0000

    11. 98 Battery Street, the OG

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_to_98 - jusqu'a 98
+* @n: nombre donner
+* Description:affiche les nombres precedent de n
+* Return:valeur 0
+*/
+void print_to_98(int n)
+
+{
+
+	
