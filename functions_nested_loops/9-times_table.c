c51edb09c58652ef8c0d42be4e8184c12a9db572
Lucas-blancaws <lblancportier@gmail.com>
2025-02-21 10:58:51 +0000

    9. Learn your times table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* times_table - table de multiplication
+* Description:affiche les tables de 0 a 9
+* Return:valeur 0
+*/
+void times_table(void)
+
+{
+	int n = 0;
+	int multi = 0;
+
+	while (n <= 9)
+	{
+		multi = 0;
+		while (multi <= 9)
+		{
+			int produit = n * multi;
+
+			if (produit >= 10)
+			{
+				_putchar (produit / 10 + '0');
+			}
+			else if (multi != 0)
+			{
+				_putchar (' ');
+			}
+			_putchar (produit % 10 + '0');
+			if (multi != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			multi++;
+		}
+		putchar ('\n');
+		n++;
+	}
+}
