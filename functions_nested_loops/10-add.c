c6590cbb3f398f66cc73935a4d0b57f59a0d3258
Lucas-blancaws <lblancportier@gmail.com>
2025-02-21 12:56:07 +0000

    10. a + b

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - addition
+ * @a: nombre 1
+ * @b: nombre 2
+ * Description ():donne la somme de 2 nombre
+ * Return: Always 0.
+ */
+
+int add(int a, int b)
+{
+	return(a + b);
+}
