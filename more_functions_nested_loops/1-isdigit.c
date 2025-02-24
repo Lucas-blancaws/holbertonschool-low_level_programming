d3b09f87f9ef148d4eff727f26deff8931aa74b3
Lucas-blancaws <lblancportier@gmail.com>
2025-02-24 10:28:18 +0000

    1. isdigit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _isdigit - minuscule ou majuscule
+* @c: 0 a 9
+* Description (): ruturn 1 si c'est en minuscule sinon 0
+* Return:valeur 0
+*/
+int _isdigit(int c)
+
+{
+	if (c >= '0' && c <= '9')
+
+	{
+		return (1);
+	}
+	{
+		return (0);
+	}
+}
