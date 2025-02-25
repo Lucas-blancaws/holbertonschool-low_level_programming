8ebd59b6b839355feecfa0dcc50245e24f6f252e
Lucas-blancaws <lblancportier@gmail.com>
2025-02-25 08:47:18 +0000

    8. You are so much sunshine in every square inch

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,10 +1,31 @@
 #include "main.h"
+#include <stdio.h>
 /**
-* print_square - carre
+* print_square - Affiche un carré d
 * @size: taille
 * Description (): carre
 * Return: valeur 0
 */
 void print_square(int size)
 {
-	int
+	int i = 0;
+	
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (i < size)
+		{
+			int j = 0;
+			i++;
+			while (j < size)
+			{
+				_putchar('#');
+				j++;
+			}
+			_putchar('\n');
+		}
+	}
+}
