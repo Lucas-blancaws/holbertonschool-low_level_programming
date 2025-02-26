3922d5a1f73fec6883441dc20cfc44e2c5ca5e34
Lucas-blancaws <lblancportier@gmail.com>
2025-02-26 09:16:15 +0000

    2. This report, by its very length, defends itself against the risk of being read

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _strlen - parcours les strings
+* @s: string
+* Description (): parcours string
+* Return:valeur 0
+*/
+int _strlen(char *s)
+{
+	int n;
+	for (n = 0; s[n]!= '\0'; n++)
+	{
+	}
+	return (n);
+}
