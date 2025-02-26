e81babf365be1ef35ce6858652487a1fb9916b52
Lucas-blancaws <lblancportier@gmail.com>
2025-02-26 14:04:35 +0000

    4. I can only go one way. I've not got a reverse gear

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -10,13 +10,13 @@ void rev_string(char *s)
 	int n, i;
 	char temp;
 
-    for (n = 0; s[n] != '\0'; n++)
-    {
-    }
-    for (i = 0; i < n / 2; i++)
-    {
-	    temp = s[i];
-	    s[i] = s[n - 1 - i];
-	    s[n - 1 - i] = temp;
-    }
+for (n = 0; s[n] != '\0'; n++)
+{
+}
+for (i = 0; i < n / 2; i++)
+{
+	temp = s[i];
+	s[i] = s[n - 1 - i];
+	s[n - 1 - i] = temp;
+}
 }
