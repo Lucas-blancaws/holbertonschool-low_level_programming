809e1428c6a49b52ad72ac68470b4a34d2c3fb7b
Lucas-blancaws <lblancportier@gmail.com>
2025-02-28 09:24:24 +0000

    6. Half the lies they tell about me aren't true

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -10,9 +10,9 @@ void puts2(char *str)
 {
 	int i;
 
-	for(i = 0; i < _strlen(str); i++)
+	for (i = 0; i < _strlen(str); i++)
 	{
-		if ( i % 2 == 0)
+		if (i % 2 == 0)
 		{
 			_putchar(str[i]);
 		}
