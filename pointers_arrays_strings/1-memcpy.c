96d32a7bf7bd02c07979a89d0b0f929828577a07
Lucas-blancaws <lblancportier@gmail.com>
2025-03-03 13:02:06 +0000

    1. memcpy

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memcpy - meme
+ * @dest: pointeur
+ * @src: pointeur
+ * @n: nombre
+ * Return: return the string value
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
