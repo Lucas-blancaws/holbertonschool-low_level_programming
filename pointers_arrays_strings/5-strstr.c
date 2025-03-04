d56a58312b4aa2dda46df66f0c4a8b46614ee9de
Lucas-blancaws <lblancportier@gmail.com>
2025-03-04 12:29:02 +0000

    5. strstr

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -9,21 +9,19 @@ char *_strstr(char *haystack, char *needle)
 {
 	if (*needle == '\0')
 		return (haystack);
+	while (haystack)
 	{
-		while (haystack)
-		{
-			char *h = haystack;
-			char *n = needle;
+		char *h = haystack;
+		char *n = needle;
 
-			while (*h && *n && (*h == *n))
-			{
-				h++;
-				n++;
-			}
-			if (*n == '\0')
-				return (haystack);
-			haystack++;
+		while (*h && *n && (*h == *n))
+		{
+			h++;
+			n++;
 		}
+		if (*n == '\0')
+			return (haystack);
+		haystack++;
 	}
 	return (NULL);
 }
