4bf110ad5933e230db31df1ff46446ec4a6b48ba
Lucas-blancaws <lblancportier@gmail.com>
2025-03-04 09:58:19 +0000

    4. strpbrk

--- pointers_arrays_strings/4-main.c
+++ pointers_arrays_strings/4-main.c
@@ -15,3 +15,22 @@
 }
 // Le programme appelle une fonction print_rev pour afficher une chaîne de caractères à l’envers.
 */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *s = "hello, world";
+    char *f = "world";
+    char *t;
+
+    t = _strpbrk(s, f);
+    printf("%s\n", t);
+    return (0);
+}
---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strpbrk - recherche ds chaîn caract� le premier caractère qui c
+ * @s : la chaîne principa
+ * @accept : la chaîne contenant les caractèr a recherch
+ * description : blab
+ * Return: return
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s)
+	{
+		char *a = accept;
+
+		for (; *a; a++)
+		{
+			if (*a == *s)
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return (NULL);
+}
