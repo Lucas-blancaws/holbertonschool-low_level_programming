be9b44cc88e8e6ecb0df8ef059740179fb4b962c
Lucas-blancaws <lblancportier@gmail.com>
2025-03-04 11:04:05 +0000

    5. strstr

--- pointers_arrays_strings/5-main.c
+++ pointers_arrays_strings/5-main.c
@@ -18,3 +18,22 @@ int main(void)
 }
 //Le programme appelle une fonction rev_string pour inverser une chaîne de caractères directement en mémoire. 
 */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *s = "hello, world";
+    char *f = "world";
+    char *t;
+
+    t = _strstr(s, f);
+    printf("%s\n", t);
+    return (0);
+}
---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strstr - localise
+ * @haystack: char
+ * @needle: char
+ * Return: returns string
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	if (*needle == '\0')
+		return (haystack);
+	{
+		while (haystack)
+		{
+			char *h = haystack;
+			char *n = needle;
+
+			while (*h && *n && (*h == *n))
+			{
+				h++;
+				n++;
+			}
+			if (*n == '\0')
+				return (haystack);
+			haystack++;
+		}
+	}
+	return (NULL);
+}
