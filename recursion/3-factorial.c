9070e682c2839a9420869bdf14dadb2760064052
Lucas-blancaws <lblancportier@gmail.com>
2025-03-05 10:56:19 +0000

    3. You mustn't be afraid to dream a little bigger, darling

---
+++ recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * factorial - factoriel
+ * @n: nombre
+ * Return: returns int
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
