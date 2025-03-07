ae753b3588eada138bee34157aafc98e542e3424
Lucas-blancaws <lblancportier@gmail.com>
2025-03-07 10:46:40 +0000

    0. It ain't what they call you, it's what you answer to

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Affiche le nom du programme
+ * @argc: Nombre d'arguments (pas  utilise)
+ * @argv: Tableau des arguments
+ */
+
+int main(int argc, char **argv)
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
