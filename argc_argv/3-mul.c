d16311b9f137ed4e4ca8f2846c0aa32ef0a2d46b
Lucas-blancaws <lblancportier@gmail.com>
2025-03-07 14:49:06 +0000

    3. Neither irony nor sarcasm is argument

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - check the code
+ * @argc: nb argument
+ * @argv: tableau argument
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int num1 = 0;
+	int num2 = 0;
+	
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	printf("%d\n", num1 * num2);
+	return (0);
+}
