659fa6310fa88648d850c3fa2436883ef6772076
Lucas-blancaws <lblancportier@gmail.com>
2025-03-13 13:56:04 +0000

    0. Poppy

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct dog - caracteristique du chien
+ * @name: char *
+ * @age: float
+ * @owner: char *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
