b5d7bac46e01df5e23a6fff0eab489cbd21eef7b
Lucas-blancaws <lblancportier@gmail.com>
2025-03-20 10:20:01 +0000

    1. To be is to be the value of a variable

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,7 +1,11 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
-
+/**
+ * print_numbers - Imprime des nombres séparés par u chaîn
+ * @separator: Chaîne entre les nombre ou NULL
+ * @n: Nombre d'entiers
+ */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
