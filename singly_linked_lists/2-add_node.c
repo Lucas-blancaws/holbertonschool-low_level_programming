d47cbcec193f75fbe618daa5cadf17b98de72e7e
Lucas-blancaws <lblancportier@gmail.com>
2025-04-01 09:55:04 +0000

    task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,9 +11,12 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	size_t len = 0;
 
 	if (!str)
 		return (NULL);
+	while (str[len])
+		len++;
 
 	new_node = malloc(sizeof(list_t));
 	if (!new_node)
@@ -25,7 +28,7 @@ list_t *add_node(list_t **head, const char *str)
 		free(new_node);
 		return (NULL);
 	}
-	new_node->len = strlen(str);
+	new_node->len = len;
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
