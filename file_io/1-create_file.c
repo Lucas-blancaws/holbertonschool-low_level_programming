93f67b7fbc5e0091571970036111a3de2b76fc4b
Lucas-blancaws <lblancportier@gmail.com>
2025-04-04 12:57:12 +0000

    task 1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+  * _strlen - returns the length of a string
+  * Description: returns the length of a string
+  * @s: string
+  * Return: length of a string
+  */
+int _strlen(char *s)
+{
+	int index = 0;
+
+	while ((*(s + index) != '\0'))
+	{
+		index++;
+	}
+	return (index);
+}
+
+/**
+ * create_file - creates a file
+ * @filename: file to create
+ * @text_content: text to write
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes_written;
+	int length = 0;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		length = _strlen(text_content);
+		bytes_written = write(fd, text_content, length);
+		if (bytes_written == -1 || bytes_written != length)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
