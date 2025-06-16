5f8e2484db9c47924221a9ef08aded5227343660
Lucas-blancaws <lblancportier@gmail.com>
2025-06-16 08:41:12 +0000

    5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - with proper grammar, but the outcome is a piece of art,
+ * Description: affiche with proper grammar, but the outcome is a piece of art
+ * Return: 0 if succeded
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
