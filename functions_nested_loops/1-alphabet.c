15f5c35b3a026471f48d188857b6395e515432d7
Lucas-blancaws <lblancportier@gmail.com>
2025-06-17 13:26:47 +0000

    1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_alphabet - lettre minuscule
+* Description: affiche les lettres minucules de la table ASCII
+* Return:valeur 0
+*/
+void print_alphabet(void)
+{
+	int c = 97;
+	
+	while (c < 123)
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -5,4 +5,5 @@
 #include <stdio.h>
 #include <unistd.h>
 int _putchar(char c);
+void print_alphabet(void);
 #endif
