76d3d70ee87057310de7ff5289d576081ee74b7a
Lucas-blancaws <lblancportier@gmail.com>
2025-06-18 12:12:48 +0000

    7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -3,11 +3,11 @@
 #include "6-abs.c"
 /**
  * print_last_digit - Affiche le dernier chiffre d'un entier
- * @n: Le nombre entier dont on veut le dernier chiffre
+ * @c: Le nombre entier dont on veut le dernier chiffre
  * Return: La valeur du dernier chiffre
  */
 int print_last_digit(int c)
 {
 	_putchar('0' + _abs(c % 10));
-	return _abs(c % 10);
+	return (_abs(c % 10));
 }
