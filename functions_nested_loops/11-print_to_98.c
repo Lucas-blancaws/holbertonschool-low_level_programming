7cc707a470abdd9bc94d460100788a98b3d6a516
Lucas-blancaws <lblancportier@gmail.com>
2025-06-19 11:57:05 +0000

    11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - print les chiifres 98
+ * Desccription: prints les chiffres 98
+ * @n: int, dernier chiffre
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i == 98)
+				printf("%d\n", i);
+			else
+				printf("%d, ", i);
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i == 98)
+				printf("%d\n", i);
+			else
+				printf("%d, ", i);
+		}
+	}
+}
