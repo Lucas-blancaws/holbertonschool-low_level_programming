f7c02536b06874b3dac3c9d10708143641fb1169
Lucas-blancaws <lblancportier@gmail.com>
2025-06-23 12:18:52 +0000

    9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+int main(void)
+{
+	int i = 1;
+	
+	for(; i <= 100; i++)
+	{
+		if (i % 3  == 0 && i % 5 == 0)
+		{
+			printf("Fizz Buzz");
+		}
+		else if (i % 3  == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5  == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
