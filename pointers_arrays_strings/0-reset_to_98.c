68dd4b2a6121856d0ddea5a02fff70901684ca8c
Lucas-blancaws <lblancportier@gmail.com>
2025-06-24 08:43:45 +0000

    0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* reset_to_98 - 98
+* @n: nombre
+* Description (): 98
+* Return:valeur 0
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+int _putchar(char c);
+void reset_to_98(int *n);
+#endif
