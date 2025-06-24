369b6e4152c43355a5bbb57b046adeec608b2706
Lucas-blancaws <lblancportier@gmail.com>
2025-06-24 08:08:25 +0000

    10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -13,20 +13,13 @@ void print_triangle(int size)
 	int c;
 
 	if (size <= 0)
-	{
 		_putchar('\n');
-		return;
-	}
 	for (a = 0; a < size; a++)
 	{
 		for (b = size - a; b > 0; b--)
-		{
 			_putchar(' ');
-		}
 		for (c = 0; c <= a; c++)
-		{
 			_putchar('#');
-		}
 		_putchar('\n');
 	}
 }
