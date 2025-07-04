e0d2adf721cf280c39ec800fb0719d5fd7010172
Lucas-blancaws <lblancportier@gmail.com>
2025-07-04 08:46:19 +0000

    betty

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -2,13 +2,13 @@
  * factorial - calcule la factorielle d'un nombre entier n
  * @n: l'entier dont on veut la factorielle
  *
- * Return : factoriel si nombre positif
- */ 
+ * Return: factoriel si nombre positif
+ */
 int factorial(int n)
 {
 	if (n < 0)
-		return -1;
+		return (-1);
 	if (n == 0)
-		return 1;
-	return n * factorial(n - 1);
+		return (1);
+	return (n * factorial(n - 1));
 }
