3041f2de516acd5a18f1cf50b7a3ee2c80895c8f
Lucas-blancaws <lblancportier@gmail.com>
2025-07-09 12:56:14 +0200

    0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * malloc_checked - Alloue de la mémoire avec malloc
+ * @b: Taille à allouer.
+ *
+ * Return: Pointeur vers la mémoire allouée
+ * Si malloc échoue return 98.
+ */
+void *malloc_checked(unsigned int b)
+{
+    void *ptr;
+
+    ptr = malloc(b);
+    if (ptr == NULL)
+        exit(98);
+
+    return (ptr);
+}
