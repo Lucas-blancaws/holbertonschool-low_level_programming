9c5ec16366d4f98c0a3ac818c303ed7adb5cb855
Lucas-blancaws <lblancportier@gmail.com>
2025-07-09 15:25:47 +0200

    2

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+/**
+ * _calloc - Alloue de la mémoire pour un tableau et l'initialise
+ * @nmemb: Nombre d'éléments
+ * @size: Taille en octets de chaque élément
+ *
+ * Return: Pointeur vers la mémoire allouée, ou NULL si erreur
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+    unsigned int i;
+    char *ptr;
+
+    if (nmemb == 0 || size == 0)
+        return (NULL);
+
+    ptr = malloc(nmemb * size);
+    if (ptr == NULL)
+        return (NULL);
+
+    for (i = 0; i < nmemb * size; i++)
+        ptr[i] = 0;
+        return (ptr);
+}
