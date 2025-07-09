48629a152102c9a82058ec085bb44195c65bb733
Lucas-blancaws <lblancportier@gmail.com>
2025-07-09 15:43:12 +0200

    2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,3 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _memset - check the code for
+* @s: pointeur
+* @b: char
+* @n: nombre
+*
+* Return: Always 0.
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+
 #include <stdlib.h>
 #include "main.h"
 /**
@@ -9,17 +31,17 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    unsigned int i;
-    char *ptr;
+        char *ptr;
 
     if (nmemb == 0 || size == 0)
         return (NULL);
 
     ptr = malloc(nmemb * size);
     if (ptr == NULL)
+    {
         return (NULL);
-
-    for (i = 0; i < nmemb * size; i++)
-        ptr[i] = 0;
-        return (ptr);
+    }
+    _memset(ptr, 0, nmemb * size);
+    
+    return (ptr);
 }
