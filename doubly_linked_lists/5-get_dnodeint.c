4e57a2366618b78ae6331282052d61dbf24b19aa
Lucas-blancaws <lblancportier@gmail.com>
2025-07-31 09:05:14 +0200

    4/5/6

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+* get_dnodeint_at_index - Get the nth node of a doubly linked list
+* @head: List head
+* @index: Index starting from 0
+*
+* Return: Node at index, or NULL
+*/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (head != NULL)
+	{
+		if (i == index)
+		return (head);
+		head = head->next;
+		i++;
+	}
+	return (NULL);
+}
